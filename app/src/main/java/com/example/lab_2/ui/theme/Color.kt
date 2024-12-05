8bc4d605a71098ace81fc985ba5794d1750cd673
IvanChyk <chykivan@gmail.com>
2024-12-05 15:17:02 +0200

    Initial commit

---
+++ app/src/main/java/com/example/lab_2/ui/theme/Color.kt
@@ -0,0 +1,11 @@
+package com.example.lab_2.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple90 = Color(0xFF311B92)
+val PurpleGrey90 = Color(0xFF1A237E)
+val Pink90 = Color(0xFF880E4F)
+
+val Purple60 = Color(0xFF4527A0)
+val PurpleGrey60 = Color(0xFF283593)
+val Pink60 = Color(0xFFAD1457)
